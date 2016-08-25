56f71c36f65442b1e0e9d1695300689aac5a4d37
steflujan <estef.lujan@gmail.com>
2016-08-25 17:41:41 -0500

    añadiendo archivos

---
+++ README.md
@@ -0,0 +1,15 @@
+#Promedio de tres números
+##Contexto: 
+Cálculo del promedio de tres números.
+##Pseudocódigo
+1. Inicio
+2. num1
+3. num2
+4. num3
+5. sumar (num1 +num2 + num3) /3
+6. promedio  
+7. FIN
+
+##Diagrama de flujo 
+
+![imagen](http://4.1m.yt/orx1Ak.jpg)
---
+++ app.js
@@ -0,0 +1,17 @@
+var num1 = prompt("Ingresa el primer numero");
+       num1 = parseInt(num1);
+var num2 = prompt("Ingresa el segundo numero");
+       num2 = parseInt(num2);
+var num3 = prompt("Ingresa el tercer numero");
+       num3 = parseInt(num3);
+var suma = num1 + num2 + num3;
+var promedio = suma/3;
+document.write("su promedio es:");
+document.write(promedio);
+/*
+var num1 = prompt ("Primer número");
+var num2 = prompt ("Segundo número");
+var num3 = prompt ("Tercer número");
+document.write("El promedio de "+ num1 + "+" + num2 + "+" + num3 + " es: ");
+document.write((parseInt(num1)+parseInt(num2)+parseInt(num3))/3);
+*/
